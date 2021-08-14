77a339bb6389e65d8e7b4b5c4cb95cd8e9fbf39c
Vasco Sousa <myemailvasco@gmail.com>
2021-08-14 15:15:34 +0100

    :recycle: Split install and configure

--- configure_omz.sh
+++ configure_omz.sh
@@ -1,5 +1,6 @@
-eval sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" > /dev/null 2>&1
 eval git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$HOME/.oh-my-zsh/themes/spaceship-prompt" --depth=1
 eval ln -s "$HOME/.oh-my-zsh/themes/spaceship-prompt/spaceship.zsh-theme" "$HOME/.oh-my-zsh/themes/spaceship.zsh-theme"
 eval git clone https://github.com/zsh-users/zsh-autosuggestions "$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions"
 eval git clone https://github.com/zsh-users/zsh-completions "$HOME/.oh-my-zsh/custom/plugins/zsh-completions"
+eval rm -rf "$HOME/.zshrc"
+eval cp ./.zshrc "$HOME"
---
+++ install_omz.sh
@@ -0,0 +1,1 @@
+eval sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
