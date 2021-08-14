36ce9d7f30bec7b9ad01fecd43e77260c4f157a8
Vasco Sousa <myemailvasco@gmail.com>
2021-08-14 15:01:22 +0100

    :bug: Removed eval zsh

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 eval sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-eval git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
-eval zsh
-eval ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+eval git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$HOME/.oh-my-zsh/themes/spaceship-prompt" --depth=1
+eval ln -s "$HOME/.oh-my-zsh/themes/spaceship-prompt/spaceship.zsh-theme" "$HOME/.oh-my-zsh/themes/spaceship.zsh-theme"
 eval rm -f "$HOME/.zshrc"
-eval cp .zshrc "$HOME"
+eval cp .zshrc "$HOME/.zshrc"
